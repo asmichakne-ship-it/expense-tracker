7a117b75f25faa06867604ca46bdb13fd7ba4dfc
Asmi Chakne <asmichakne@gmail.com>
2026-06-13 12:39:48 +0530

    Update ExpenseTrackerApplication.java

--- ExpenseTrackerApplication.java
+++ ExpenseTrackerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExpenseTrackerApplication {
-    public static void main(String[] args) {
+public class ExpenseTrackerApplication 
+{
+    public static void main(String[] args) 
+    {
         SpringApplication.run(ExpenseTrackerApplication.class, args);
     }
 }
